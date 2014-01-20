8626232b4d38a7eb434a19aa590132632c0cd8f4
Maxim <maxim.kitsenko.r31@gmail.com>
2014-01-20 21:40:40 +0400

    Add E-bay data structures

---
+++ src/EbayAccess/Models/EbayGetOrdersResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EbayAccess.Models
+{
+	public class EbayGetOrdersResponse
+	{
+		public DateTime Timestamp { get; set; }
+
+		public string Ack { get; set; }
+
+		public string Version { get; set; }
+
+		public string Build { get; set; }
+
+		public PaginationResult PaginationResult { get; set; }
+	}
+
+	public class PaginationResult
+	{
+		public int TotalNumberOfPages { get; set; }
+
+		public int TotalNumberOfEntries { get; set; }
+
+		public List<EbayOrder> OrderArray { get; set; }
+
+		public int OrdersPerPage { get; set; }
+
+		public int PageNumber { get; set; }
+
+		public int ReturnedOrderCountActual { get; set; }
+	}
+}
--- src/EbayAccess/Models/EbayOrder.cs
+++ src/EbayAccess/Models/EbayOrder.cs
@@ -1,6 +1,67 @@
-﻿namespace EbayAccess.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace EbayAccess.Models
 {
 	public sealed class EbayOrder
 	{
+		public string OrderId { get; set; }
+
+		public string OrderStatus { get; set; }
+
+		public CheckoutStatus CheckoutStatus { get; set; }
+
+		public DateTime CreatedTime { get; set; }
+
+		public string PaymentMethods { get; set; }
+
+		public List<Transaction> TransactionArray { get; set; }
+
+		public string BuyerUserId { get; set; }
+	}
+
+	public class CheckoutStatus
+	{
+		public string EBayPaymentStatus { get; set; }
+
+		public DateTime LastModifiedTime { get; set; }
+
+		public string PaymentMethod { get; set; }
+
+		public string Status { get; set; }
+
+		public bool IntegratedMerchantCreditCardEnabled { get; set; }
+	}
+
+	public class Transaction
+	{
+		public Buyer Buyer { get; set; }
+
+		public Item Item { get; set; }
+
+		public int QuantityPurchased { get; set; }
+
+		public int TransactionId { get; set; }
+
+		public int TransactionPrice { get; set; }
+
+		public string CurrencyId { get; set; }
+
+		public string OrderLineItemId { get; set; }
+	}
+
+	public class Item
+	{
+		public int ItemId { get; set; }
+
+		public string Site { get; set; }
+
+		public string Title { get; set; }
+	}
+
+	public class Buyer
+	{
+		public string Email { get; set; }
 	}
 }
