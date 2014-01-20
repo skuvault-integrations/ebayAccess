a17a8aa25c07eda27460b9a0211dc5326ec990a5
Maxim <maxim.kitsenko.r31@gmail.com>
2014-01-20 23:37:43 +0400

    Add tests (Red)

---
+++ src/EbayAccessTests/Services/EbayOrdersParserTest.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using EbayAccess.Services;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EbayAccessTests.Services
+{
+	[TestFixture]
+	public class EbayOrdersParserTest
+	{
+		[Test]
+		public void FileStreamWithCorrectXml_ParseOrdersResponse_HookupCorrectDeserializedObject()
+		{
+			using (var fs = new FileStream(@".\EbayServiceGetOrdersResponseWithItems.xml", FileMode.Open, FileAccess.Read))
+			{
+				var parser = new EbayOrdersParser();
+				var orders = parser.ParseOrdersResponse(fs);
+				orders.Should().HaveCount(1, "because in source file there is {0} order",1);
+			}
+		}
+	}
+}
