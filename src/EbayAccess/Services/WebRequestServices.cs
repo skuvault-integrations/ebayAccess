363f2dc9dbd6d5995d446a2dbbe627f3a97d911a
Maxim <maxim.kitsenko.r31@gmail.com>
2014-01-21 12:26:22 +0400

    Refector: pass date and credentials as parameters

--- src/EbayAccess/Services/WebRequestServices.cs
+++ src/EbayAccess/Services/WebRequestServices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -80,9 +81,9 @@ namespace EbayAccess.Services
 				string body =
 					string.Format(
 						"<?xml version=\"1.0\" encoding=\"utf-8\"?><GetOrdersRequest xmlns=\"urn:ebay:apis:eBLBaseComponents\"><RequesterCredentials><eBayAuthToken>{0}</eBayAuthToken></RequesterCredentials><CreateTimeFrom>{1}</CreateTimeFrom><CreateTimeTo>{2}</CreateTimeTo></GetOrdersRequest>​",
-						"AgAAAA**AQAAAA**aAAAAA**Z6PZUg**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wFk4GhC5eEpg2dj6x9nY+seQ**OX8CAA**AAMAAA**SEIoL5SqnyD4fbOhrRTCxShlrCVPyQEp4R++AkBuR3abexAYvgHkUOJvJ6EIBNvqCyDj9MTbIuft2lY/EJyWeze0NG/zVa1E3wRagdAOZXYGnSYaEJBkcynOEfQ7J8vEbG4dd1NoKixUBARbVH9jBoMHTuDy8Bj36NNvr5/iQbaMm+VnGgezBeerdl5S8M/5EzLpbYk1l6cRWJRmVN41fY/ERwj6dfNdD1JqKnDmuGXjVN4KF4k44UKkAv9Zigx+QWJgXOTFCvbwL8iXni079cZNwL35YA6NC2O8IDm7TKooJwsUhbWjNWO2Rxb5MowYS8ls1X/SRZ4VcRDYnnaeCzhLsUTOGCoUvsKumXn3WkGJhLD7CH671suim3vrl9XB+oyCev22goM3P7wr5uhMknN4mxE178Pyd0F/X2+DbfxgpJyVs/gBV7Ym11bGC6wmPHZO2zSSqVIKdkmLf0Uw8q/aqUEiHDVl8IwuvVXsW7hCbZeBkdRzr5JEkuI0FYZ8e3WS5BcGrvcEJaC0ZjMxAW/LkFktQooy9UckjWp/6l+rVKgeJYsCik/OrPWJKVmekBSUeKYEmm/Mo5QeU6Hqlrz+S3m+WR2NOyc8F0Wqk2zDTNpLlAh/RbhmUoHtmLtdgu9ESwBWz0L9B11ME3rB7udeuaEf9Rd48H77pZ1UKoK9C7mrJMHFNSvLG1Gq6SCWe2KxDij7DvKe5vYmy2rS1sdJDCfBq0GFnUBZOmh+N64KqxkIUY26nPeqm/KoqQ7R",
-						"2014-01-01T08:00:00.000Z",
-						"2014-01-21T08:00:00.000Z");
+						this._credentials.Token,
+						dateFrom.ToString("O").Substring(0, 23) + "Z",
+						dateTo.ToString("O").Substring(0, 23) + "Z");
 
 				var request = this.CreateServicePostRequest(url, body, headers);
 				using (var response = (HttpWebResponse) request.GetResponse())
@@ -91,14 +92,6 @@ namespace EbayAccess.Services
 					{
 						result = new EbayOrdersParser().ParseOrdersResponse(dataStream);
 					}
-					//Console.WriteLine(response.StatusDescription);
-					//Stream dataStream = response.GetResponseStream();
-					//StreamReader reader = new StreamReader(dataStream);
-					//string responseFromServer = reader.ReadToEnd();
-					
-					//reader.Close();
-					//dataStream.Close();
-					//response.Close();
 				}
 
 				return result;
