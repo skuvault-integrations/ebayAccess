9d346d78bf69a0ee0bc8a1f3038582ce49caae0f
MKitsenko <maxim.kitsenko.r31@gmail.com>
2014-02-20 14:47:46 +0400

    Ebay factory added

---
+++ src/EbayAccess/EbayFactory.cs
@@ -0,0 +1,17 @@
+﻿using EbayAccess.Models;
+
+namespace EbayAccess
+{
+	public interface IEbayFactory
+	{
+		IEbayService CreateService(EbayCredentials credentials);
+	}
+
+	public sealed class EbayFactory:IEbayFactory
+	{
+		public IEbayService CreateService(EbayCredentials credentials)
+		{
+			return new EbayService(credentials, "https://api.sandbox.ebay.com/ws/api.dll");
+		}
+	}
+}
