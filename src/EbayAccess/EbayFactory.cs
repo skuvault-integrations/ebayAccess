a929ca3130da709b75199d51d7de76ef9fb1842a
Dmitry Sobornykov <d1984v@gmail.com>
2014-02-21 13:13:01 +0200

    moved devcredentials to the factory constructor

--- src/EbayAccess/EbayFactory.cs
+++ src/EbayAccess/EbayFactory.cs
@@ -1,18 +1,28 @@
-﻿using EbayAccess.Models;
+﻿using CuttingEdge.Conditions;
+using EbayAccess.Models;
 using EbayAccess.Services;
 
 namespace EbayAccess
 {
 	public interface IEbayFactory
 	{
-		IEbayService CreateService(EbayUserCredentials userCredentials, EbayDevCredentials ebayDevCredentials);
+		IEbayService CreateService( EbayUserCredentials userCredentials );
 	}
 
-	public sealed class EbayFactory:IEbayFactory
+	public sealed class EbayFactory : IEbayFactory
 	{
-		public IEbayService CreateService(EbayUserCredentials userCredentials, EbayDevCredentials ebayDevCredentials)
+		private readonly EbayDevCredentials _devCredentials;
+
+		public EbayFactory( EbayDevCredentials devCredentials )
 		{
-			return new EbayService(userCredentials, ebayDevCredentials);
+			Condition.Requires( devCredentials, "devCredentials" ).IsNotNull();
+
+			this._devCredentials = devCredentials;
+		}
+
+		public IEbayService CreateService( EbayUserCredentials userCredentials )
+		{
+			return new EbayService( userCredentials, this._devCredentials );
 		}
 	}
 }
