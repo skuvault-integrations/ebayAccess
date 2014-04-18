61a6b65336288999c9c141c26be61336a851e5c9
MaxKits <maxim.kitsenko.r31@gmail.com>
2014-04-18 15:36:02 +0400

    added extension to get date in iso 8601

---
+++ src/EbayAccess/Infrastructure/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml;
+
+namespace EbayAccess.Infrastructure
+{
+	public static class Extensions
+	{
+		public static string ToStringUtcIso8601( this DateTime dateTime )
+		{
+			var universalTime = dateTime.ToUniversalTime();
+			var result = XmlConvert.ToString( universalTime, XmlDateTimeSerializationMode.RoundtripKind );
+			return result;
+		}
+	}
+}
---
+++ src/EbayAccessTests/Infrastructure/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EbayAccess.Infrastructure;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EbayAccessTests.Infrastructure
+{
+	[ TestFixture ]
+	public class Extensions
+	{
+		/// <summary>
+		/// Ebay accepts format: YYYY-MM-DDTHH:MM:SS.SSSZ
+		/// </summary>
+		[ Test ]
+		public void ToStringIso8601_DateTimeInUtc_StringRepresenatOfDateTimeInUtcEbay8601Format()
+		{
+			//A
+			var testDate = new DateTime( 2004, 8, 4, 19, 09, 02, 768, DateTimeKind.Utc );
+
+			//A
+			var testDateIso8601StringRepresentation = testDate.ToStringUtcIso8601();
+
+			//A
+			testDateIso8601StringRepresentation.Should().Be( "2004-08-04T19:09:02.768Z", "It is the right representation of iso 8601 format" );
+		}
+	}
+}
