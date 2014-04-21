5149045e115b89c8a275ab2f80890dd7dacb57ac
MaxKits <maxim.kitsenko.r31@gmail.com>
2014-04-21 13:11:07 +0400

    pages parser derived from abstract xml parser

--- src/EbayAccess/Services/Parsers/EbayPagesParser.cs
+++ src/EbayAccess/Services/Parsers/EbayPagesParser.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
 using System.Xml.Linq;
@@ -8,36 +7,29 @@ using EbayAccess.Models.BaseResponse;
 
 namespace EbayAccess.Services.Parsers
 {
-	public class EbayPagesParser
+	public class EbayPagesParser : AbstractXmlParser
 	{
 		public PaginationResult ParsePaginationResultResponse( Stream stream, bool keepStremPosition = true )
 		{
 			try
 			{
-				//
-				//var streamReader = new StreamReader( stream );
-				//string tempstr = streamReader.ReadToEnd();
-				//stream.Position = 0;
-				//
-
 				PaginationResult res = null;
 
 				XNamespace ns = "urn:ebay:apis:eBLBaseComponents";
 
 				var root = XElement.Load( stream );
 
-				object temp = null;
-
 				var paginationResultElement = root.Element( ns + "PaginationResult" );
 				if( paginationResultElement != null )
 				{
 					res = new PaginationResult();
 
-					if( this.GetElementValue( paginationResultElement, ref temp, ns, "TotalNumberOfPages" ) )
-						res.TotalNumberOfPages = int.Parse( ( string )temp );
+					string temp;
+					if( !string.IsNullOrWhiteSpace( temp = GetElementValue( paginationResultElement, ns, "TotalNumberOfPages" ) ) )
+						res.TotalNumberOfPages = int.Parse( temp );
 
-					if( this.GetElementValue( paginationResultElement, ref temp, ns, "TotalNumberOfEntries" ) )
-						res.TotalNumberOfEntries = int.Parse( ( string )temp );
+					if( !string.IsNullOrWhiteSpace( temp = GetElementValue( paginationResultElement, ns, "TotalNumberOfEntries" ) ) )
+						res.TotalNumberOfEntries = int.Parse( temp );
 				}
 
 				if( keepStremPosition )
@@ -53,30 +45,6 @@ namespace EbayAccess.Services.Parsers
 				var bufferStr = utf8Encoding.GetString( buffer );
 				throw new Exception( "Can't parse: " + bufferStr, ex );
 			}
-		}
-
-		private bool GetElementValue( XElement x, ref object parsedElement, XNamespace ns, params string[] elementName )
-		{
-			if( elementName.Length > 0 )
-			{
-				var element = x.Element( ns + elementName[ 0 ] );
-				if( element != null )
-				{
-					if( elementName.Length > 1 )
-						return this.GetElementValue( element, ref parsedElement, ns, elementName.Skip( 1 ).ToArray() );
-					parsedElement = element.Value;
-					return true;
-				}
-			}
-
-			return false;
-		}
-
-		private object GetElementValue( XElement x, XNamespace ns, params string[] elementName )
-		{
-			object parsedElement = null;
-			this.GetElementValue( x, ref parsedElement, ns, elementName );
-			return parsedElement;
 		}
 
 		public PaginationResult ParsePaginationResultResponse( WebResponse response )
