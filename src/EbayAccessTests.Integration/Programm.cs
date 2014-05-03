f36612a5c656641086e412755ca57927b8a29eaa
MaxKits <macros5@ya.ru>
2014-05-03 22:42:56 +0400

    added common test for getting variations skus on GetOrdersAsync

--- src/EbayAccessTests.Integration/Programm.cs
+++ src/EbayAccessTests.Integration/Programm.cs
@@ -39,5 +39,21 @@ namespace EbayAccessTests.Integration
 			//------------ Assert
 			orders.First().Variations.TrueForAll( x => !string.IsNullOrWhiteSpace( x.Sku ) ).Should().BeTrue( "because on site there is 2 orders" );
 		}
+
+		[ Test ]
+		public void GetOrders_EbayServiceWithOrdersVariationsSku_HookupProductsVariationsSku()
+		{
+			//------------ Arrange
+			var ebayFactory = new EbayFactory( this._credentials.GetEbayDevCredentials() );
+			var ebayService = ebayFactory.CreateService( this._credentials.GetEbayUserCredentials(), this._credentials.GetEbayEndPoint() );
+
+			//------------ Act
+			var ordersTask = ebayService.GetOrdersAsync( new DateTime( 2014, 5, 2, 18, 0, 0 ), new DateTime( 2014, 5, 3, 10, 0, 0 ) );
+			ordersTask.Wait();
+			var orders = ordersTask.Result;
+
+			//------------ Assert
+			orders.First().TransactionArray.TrueForAll( x => x.Variation != null && !string.IsNullOrWhiteSpace( x.Variation.Sku ) ).Should().BeTrue( "because on site there is 2 orders" );
+		}
 	}
 }
