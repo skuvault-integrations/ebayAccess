0a176baad3cfb5e52399c1b322361b636acf1f81
MaxKits <macros5@ya.ru>
2014-05-29 16:25:19 +0400

    log auth message only in trace mode

--- src/EbayAccess/Models/BaseResponse/EbayBaseResponse.cs
+++ src/EbayAccess/Models/BaseResponse/EbayBaseResponse.cs
@@ -24,7 +24,16 @@ namespace EbayAccess.Models.BaseResponse
 			set
 			{
 				if( value != null )
-					EbayLogger.Log().Error( "[ebay] An error occured in response: code={0}, message={1}", value.ErrorCode, value.LongMessage );
+					switch( value.ErrorCode )
+					{
+						// Auth exception must appears only in trace
+						case "21916017":
+							EbayLogger.Log().Trace( "[ebay] An error occured in response: code={0}, message={1}", value.ErrorCode, value.LongMessage );
+							break;
+						default:
+							EbayLogger.Log().Error( "[ebay] An error occured in response: code={0}, message={1}", value.ErrorCode, value.LongMessage );
+							break;
+					}
 				this._error = value;
 			}
 		}
