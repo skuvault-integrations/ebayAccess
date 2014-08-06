1ca086686eb9833bb85e186e9d6f4aa69fe4fbf8
MaxKits <macros5@ya.ru>
2014-08-07 01:55:50 +0400

    Added test for ebay error parser

---
+++ src/EbayAccessTests/Services/Parsers/EbayXmlParserTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.IO;
+using System.Xml.Linq;
+using EbayAccess.Services.Parsers;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EbayAccessTests.Services.Parsers
+{
+	[ TestFixture ]
+	public class EbayXmlParserTest : EbayXmlParser< object >
+	{
+		[ Test ]
+		public void Parse_GetItemResponseWithSku_HookupSku()
+		{
+			//A
+			using( var fs = new FileStream( @".\Files\EbayServiceGetAnyResponseWithMultipleErrors.xml", FileMode.Open, FileAccess.Read ) )
+			{
+				XNamespace ns = "urn:ebay:apis:eBLBaseComponents";
+
+				var root = XElement.Load( fs );
+
+				//A
+				var errors = this.ResponseContainsErrors( root, ns ) as IList;
+
+				//A
+				errors.Count.Should().Be( 2 );
+			}
+		}
+	}
+}
