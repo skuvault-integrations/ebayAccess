7aeadce654e2ae1c61665e0a831ce31605f40ed1
MaxKits <macros5@ya.ru>
2014-08-27 20:17:05 +0400

    added class to sotre ebay errors

---
+++ src/EbayAccess/Models/EbayErrors.cs
@@ -0,0 +1,21 @@
+﻿using EbayAccess.Models.BaseResponse;
+
+namespace EbayAccess.Models
+{
+	public static class EbayErrors
+	{
+		public static ResponseError EbayPixelSizeError
+		{
+			get
+			{
+				return new ResponseError
+				{
+					ErrorCode = "21919137",
+					LongMessage = "Buyers love large photos that clearly show the item, so please upload high-resolution photos that are at least 500 pixels on the longest side",
+					ShortMessage = "Buyers love large photos that clearly show the item, so please upload high-resolution photos that are at least 500 pixels on the longest side",
+					SeverityCode = "Error"
+				};
+			}
+		}
+	}
+}
