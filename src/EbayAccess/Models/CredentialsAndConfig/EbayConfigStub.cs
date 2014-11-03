37ab2242af0f64d6bcf2a1f527dd03f896c875e3
MaxKits <macros5@ya.ru>
2014-11-03 13:50:17 +0300

    ebay config  stub added

--- src/EbayAccess/Models/CredentialsAndConfig/EbayConfigStub.cs
+++ src/EbayAccess/Models/CredentialsAndConfig/EbayConfigStub.cs
@@ -1,10 +1,9 @@
-﻿using EbayAccess.Models.CredentialsAndConfig;
-
-namespace EbayAccessTests.Integration.TestEnvironment
+﻿namespace EbayAccess.Models.CredentialsAndConfig
 {
 	public class EbayConfigStub : EbayConfig
 	{
-		public EbayConfigStub( string appName, string devName, string certName ) : base( appName, devName, certName )
+		public EbayConfigStub( string appName, string devName, string certName )
+			: base( appName, devName, certName )
 		{
 		}
 
@@ -16,19 +15,16 @@ namespace EbayAccessTests.Integration.TestEnvironment
 		public override string EndPoint
 		{
 			get { return "https://api.sandbox.ebay.com/ws/api.dll"; }
-			//get { return "https://api.ebay.com/ws/api.dll"; }
 		}
 
 		public override string SignInUrl
 		{
 			get { return "https://signin.sandbox.ebay.com/ws/eBayISAPI.dll"; }
-			//get { return "https://signin.ebay.com/ws/eBayISAPI.dll"; }
 		}
 
 		public override string EndPointBulkExhange
 		{
 			get { return "https://webservices.sandbox.ebay.com/BulkDataExchangeService"; }
-			//get { return "https://storage.sandbox.ebay.com/FileTransferService"; }
 		}
 	}
 }
