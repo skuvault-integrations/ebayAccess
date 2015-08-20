e5a754c74752ed9cde427ff5ca711dc197f21dc8
MaxKits <maxim.kitsenko.r31@gmail.com>
2015-08-20 16:01:35 +0300

    test configured for sandbox

--- src/EbayAccessTests.Integration/Program.cs
+++ src/EbayAccessTests.Integration/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Linq;
 using EbayAccess;
 using EbayAccess.Misc;
+using EbayAccess.Models;
 using EbayAccess.Models.ReviseInventoryStatusRequest;
 using EbayAccessTests.Integration.TestEnvironment;
 using FluentAssertions;
@@ -185,7 +186,7 @@ namespace EbayAccessTests.Integration
 		{
 			////Attention!!! This code will regenerate youe credentials!!!
 			//------------ Arrange
-			var ebayFactory = new EbayFactory( this._credentials.GetEbayConfigProduction() );
+			var ebayFactory = new EbayFactory( this._credentials.GetEbayConfigSandbox() );
 			var ebayService = ebayFactory.CreateService();
 
 			//------------ Act
@@ -210,22 +211,71 @@ namespace EbayAccessTests.Integration
 			var ebayService = ebayFactory.CreateService( this._credentials.GetEbayUserCredentials() );
 
 			//------------ Act
-			var activeproductsTask = ebayService.GetActiveProductsAsync();
+			var activeproductsTask = ebayService.GetActiveProductsAsync( true );
 			activeproductsTask.Wait();
 			var activeProducts = activeproductsTask.Result;
 
-			var updateResultsaTask1 = ebayService.ReviseInventoriesStatusAsync( activeProducts.Select( x => new InventoryStatusRequest { ItemId = x.ItemId.ToLong(), Quantity = 10050, Sku = x.Sku } ) );
-			updateResultsaTask1.Wait();
-			var updateResultsa1 = updateResultsaTask1.Result;
+			var ids = new List<string> { "110143658380",
+"131430619451",
+"271747038084a",
+"271678623583a",
+"271678911379a",
+"281637200703a",
+"281637201281a",
+"271813925325a",
+"271813926032a",
+"281637202546a",
+"271813926395a",
+"271813926591a",
+"271813927335a",
+"281637203969a",
+"271813928019a",
+"281637204330a",
+"281637204413a",
+"271813928885a",
+"271813929675a",
+"281637205393a",
+"271813930167a",
+"281637205820a",
+"281637206322a",
+"271813931798a",
+"281637206956a",
+"271813935475a",
+"271813938364a",
+"281199745841a",
+"271882280575a",
+"271788708836a",
+"281201163068a",
+"281517254422a",
+"281518276002a",
+"271887995344a",
+"281710345060a",
+"281710352452a",
+"271739351783a",
+"271742387348a",
+"271406349292a" };
+			//activeProducts = activeProducts.Where( x => ids.Contains( x.ItemId ) );
+			activeProducts = activeProducts.Take( 10 );
 
-			var updateResultsaTask2 = ebayService.ReviseInventoriesStatusAsync( activeProducts.Select( x => new InventoryStatusRequest { ItemId = x.ItemId.ToLong(), Quantity = 100, Sku = x.Sku } ) );
-			updateResultsaTask1.Wait();
-			var updateResultsa2 = updateResultsaTask2.Result;
+			var updateResultsaTask12 = ebayService.UpdateInventoryAsync( activeProducts.Where( x => x.GetSku( false ) != null ).Select( x => new UpdateInventoryRequest { ItemId = x.ItemId.ToLong(), Quantity = 1, Sku = x.GetSku( false ).Sku, ConditionID = x.ConditionId, IsVariation = x.IsItemWithVariations() } ) );
+			updateResultsaTask12.Wait();
+			var updateResultsa12 = updateResultsaTask12.Result;
+
+			//var updateResultsaTask1 = ebayService.ReviseInventoriesStatusAsync( activeProducts.Select( x => new InventoryStatusRequest { ItemId = x.ItemId.ToLong(), Quantity = x.Quantity, Sku = x.GetSku().Sku } ) );
+			//updateResultsaTask1.Wait();
+			//var updateResultsa1 = updateResultsaTask1.Result;
+
+			//var updateResultsaTask2 = ebayService.ReviseInventoriesStatusAsync( activeProducts.Select( x => new InventoryStatusRequest { ItemId = x.ItemId.ToLong(), Quantity = x.Quantity, Sku = x.GetSku().Sku } ) );
+			//updateResultsaTask1.Wait();
+			//var updateResultsa2 = updateResultsaTask2.Result;
 
 			//------------ Assert
-			activeProducts.Count().Should().Be( 4999 );
-			updateResultsa1.Count().Should().Be( 1250 );
-			updateResultsa2.Count().Should().Be( 1250 );
+			//activeProducts.Count().Should().Be( 4999 );
+			//updateResultsa1.Count().Should().Be( 1250 );
+
+			System.Threading.Tasks.Task.Delay(150000);
+
+			updateResultsa12.Count().Should().Be(1250);
 		}
 	}
 }
