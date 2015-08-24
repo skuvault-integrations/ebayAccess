bccc4f5a3b6b4d08b079c4710309bd3ec7adbe6e
MaxKits <maxim.kitsenko.r31@gmail.com>
2015-08-24 09:08:46 +0300

    dependency added to build script

--- .build.ps1
+++ .build.ps1
@@ -114,6 +114,7 @@ task NuGet Package, Version, {
 			<group targetFramework="net45">
 				<dependency id="Netco" version="1.3.1" />
 				<dependency id="CuttingEdge.Conditions" version="1.2.0.0" />
+				<dependency id="Newtonsoft.Json" version="7.0.1" />
 			</group>
 		</dependencies>
 	</metadata>
